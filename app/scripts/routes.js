84740da361d05b26eac72f95cba493b7b714cb1b
Will <will@willinchina.com>
2016-01-18 03:43:33 +0800

    Removed angular hash bang by enabling HTML5 Mode in routes.js and adding base to index.html

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -9,16 +9,30 @@
  */
 angular.module('blabDashApp')
 
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
+.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
 
-      .when('/stats', {
-        templateUrl: 'views/stats.html',
-        controller: 'StatsCtrl'
-      })
-      .otherwise({redirectTo: '/'});
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+
+        .when('/stats', {
+                templateUrl: 'views/stats.html',
+                controller: 'StatsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+
+        if (window.history && window.history.pushState) {
+            $locationProvider.html5Mode({
+                enabled: true,
+            });
+        }
+
+
   }]);
